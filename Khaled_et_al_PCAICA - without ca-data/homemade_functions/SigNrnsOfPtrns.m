016cd5b83c6a9bd71aa113e2ff3452307001d364
Masanori Nomoto <67352253+masanori-nomoto@users.noreply.github.com>
2025-03-03 15:31:34 +0900

    Delete Khaled_et_al_PCAICA - without ca-data/homemade_functions directory

--- Khaled_et_al_PCAICA - without ca-data/homemade_functions/SigNrnsOfPtrns.m
+++
@@ -1,7 +0,0 @@
-SigPtrnNo = [1:5,8:20,22:27,29,30];
-SigPtrn = result_MPPCA.neuron_weight_sigID(:,SigPtrnNo);
-[rows_SigPtrn,cols_SigPtrn] = find(SigPtrn == 1);
-SeqSigPtrn = sort(rows_SigPtrn);
-%% Excel
-% % Export as an excel file
-% xlswrite('SeqSigPtrn.xlsx',SeqSigPtrn)
